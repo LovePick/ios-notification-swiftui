c707093ac526f1f468e855dc5d0bd0442ee887ac
pick <picknavin556@gmail.com>
2022-09-11 16:32:33 +0700

    init

---
+++ TestNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestNotification
+//
+//  Created by Supapon Pucknavin on 11/9/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestNotification/TestNotificationApp.swift
@@ -0,0 +1,19 @@
+//
+//  TestNotificationApp.swift
+//  TestNotification
+//
+//  Created by Supapon Pucknavin on 11/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestNotificationApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: MyAppdelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
